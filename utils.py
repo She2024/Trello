0ae241ed5a9bbe2843f06c779f23a7a1e57fed85
she2024 <bullybluedog@hotmail.com>
2024-09-07 10:40:41 +1000

    added error handling

--- utils.py
+++ utils.py
@@ -5,22 +5,22 @@ import functools
 from init import db
 from models.user import User
 
-def authorise_as_admin():
-    # get the user's id from get_jwt_identity
-    user_id = get_jwt_identity()
-    # fetch the user from the db
-    stmt = db.select(User).filter_by(id=user_id)
-    user = db.session.scalar(stmt)
-    # check whether the user is an admin or not
-    return user.is_admin
+# def authorise_as_admin():
+#     # get the user's id from get_jwt_identity
+#     user_id = get_jwt_identity()
+#     # fetch the user from the db
+#     stmt = db.select(User).filter_by(id=user_id)
+#     user = db.session.scalar(stmt)
+#     # check whether the user is an admin or not
+#     return user.is_admin
 
-# Creating a decorator for autotise_as_admin
+# Creating a decorator for authorise_as_admin
 
 def auth_as_admin_decorator(fn):
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
         # get the user's id from get_jwt_identity
-        user_id = get_jwt_identity
+        user_id = get_jwt_identity()
         # fetch the user from the db
         stmt = db.select(User).filter_by(id=user_id)
         user = db.session.scalar(stmt)
@@ -28,12 +28,9 @@ def auth_as_admin_decorator(fn):
         if user.is_admin:
             # allow the decorator fn to execute
             return fn(*args, **kwargs)
-        # else 
+        # else
         else:
             # return error
             return {"error": "Only admin can perform this action"}, 403
-   
+    
     return wrapper
-
-# /auth/users/user_id
-@auth_db.route("/users/<int:user_id>", methods=["DELETE"])
