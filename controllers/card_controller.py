1c160be51731ab0dd3ea5546c1ead8e4d789da28
she2024 <bullybluedog@hotmail.com>
2024-09-07 10:25:16 +1000

    Created auth as admin decorator and user controller

--- controllers/card_controller.py
+++ controllers/card_controller.py
@@ -1,117 +1,54 @@
-from datetime import date
+from init import db, ma
+from marshmallow import fields, validates
+from marshmallow.validate import Length, And, Regexp, OneOf
+from marshmallow.exceptions import ValidationError
 
-from flask import Blueprint, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+VALID_STATUSES = ("To Do", "In Progress", "Completed", "Testing", "Deployed")
+VALID_PRIORITIES = ("Low", "Medium", "High", "Immediate")
 
-from init import db
-from models.card import Card, card_schema, cards_schema
+class Card(db.Model):
+    __tablename__ = "cards"
 
-from controllers.comment_controller import comments_bp
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    status = db.Column(db.String)
+    priority = db.Column(db.String)
+    date = db.Column(db.Date) # Created Date
 
-from utils import authorise_as_admin
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
 
-cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
-cards_bp.register_blueprint(comments_bp)
+    user = db.relationship('User', back_populates='cards')
+    comments = db.relationship('Comment', back_populates='card', cascade="all, delete")
 
-# /cards - GET - fetch all cards
-# /cards/<id> - GET - fetch a specific card
-# /cards - POST - create a new card
-# /cards/<id> - DELETE - delete a card
-# /cards/<id> - PUT, PATCH - edit a card entry
 
-# /cards - GET - fetch all cards
-@cards_bp.route("/")
-def get_all_cards():
-    stmt = db.select(Card).order_by(Card.date.desc())
-    cards = db.session.scalars(stmt)
-    return cards_schema.dump(cards)
+class CardSchema(ma.Schema):
+    user = fields.Nested('UserSchema', only=["id", "name", "email"])
+    comments = fields.List(fields.Nested('CommentSchema', exclude=['card']))
 
-# /cards/<id> - GET - fetch a specific card
-@cards_bp.route("/<int:card_id>")
-def get_a_card(card_id):
-    stmt = db.select(Card).filter_by(id=card_id)
-    # stmt = db.select(Card).where(Card.id==card_id)
-    card = db.session.scalar(stmt)
-    if card:
-        return card_schema.dump(card)
-    else:
-        return {"error": f"Card with id '{card_id}' not found"}, 404
-    
-# /cards - POST - create a new card
-@cards_bp.route("/", methods=["POST"])
-@jwt_required()
-def create_card():
-    # get the data from the body of the request
-    body_data = card_schema.load(request.get_json())
-    # create a new card model instance
-    card = Card(
-        title = body_data.get("title"),
-        description = body_data.get("description"),
-        date = date.today(),
-        status = body_data.get("status"),
-        priority = body_data.get("priority"),
-        user_id = get_jwt_identity()
-    )
-    # add and commit to the DB
-    db.session.add(card)
-    db.session.commit()
-    # response message
-    return card_schema.dump(card)
+    title = fields.String(required=True, validate=And(Length(min=4, error="Title must be at least 4 characters in length."), Regexp("^[A-Z][A-Za-z0-9 ]+$", error="Title must start with a capital letter and have alphanumeric characters only.")))
 
-# /cards/<id> - DELETE - delete a card
-@cards_bp.route("/<int:card_id>", methods=["DELETE"])
-@jwt_required()
-def delete_card(card_id):
-    # check whether the user is admin or not
-    is_admin = authorise_as_admin()
-    # if not admin
-    if not is_admin:
-        # return error message
-        return {"error": "User is not authorized to perform this action."}
+    status = fields.String(validate=OneOf(VALID_STATUSES))
 
-    # fetch the card from the database
-    stmt = db.select(Card).filter_by(id=card_id)
-    card = db.session.scalar(stmt)
-    # if card exists
-    if card:
-        # delete the card
-        db.session.delete(card)
-        db.session.commit()
-        return {"message": f"Card {card.title} deleted successfully!"}
-    # else
-    else:
-        # return error message
-        return {"error": f"Card with id {card_id} not found"}, 404
+    priority = fields.String(validate=OneOf(VALID_PRIORITIES, error="Invalid Priority Selected."))
 
-# /cards/<id> - PUT, PATCH - edit a card entry
-@cards_bp.route("/<int:card_id>", methods=["PUT", "PATCH"])
-@jwt_required()
-def update_card(card_id):
-    # get the info from the body of the request
-    body_data = card_schema.load(request.get_json(), partial=True)
-    # get the card from the database
-    stmt = db.select(Card).filter_by(id=card_id)
-    card = db.session.scalar(stmt)
-    # check whether the user is admin or not
-    is_admin = authorise_as_admin()
-    # if the card exists
-    if card:
-        # if the user is not the owner of the card
-        if not is_admin or str(card.user_id) != get_jwt_identity():
-            # return error message
-            return {"error": "Cannot perform this operation. Only owners are allowed to execute this operation."}
+    @validates("status")
+    def validate_status(self, value):
+        # if trying to see the value of status as "In Progress"
+        if value == VALID_STATUSES[1]:
+            # check whether an existing In Progress card exists or not
+            # SELECT COUNT(*) FROM table_name WHERE status="In Progress"
+            stmt = db.select(db.func.count()).select_from(Card).filter_by(status=VALID_STATUSES[1])
+            count = db.session.scalar(stmt)
+            # if it exists
+            if count > 0:
+                # send error message
+                raise ValidationError("You already have an In-Progress card.")
+                          
+    class Meta:
+        fields = ("id", "title", "description", "status", "priority", "date", "user", "comments")
+        ordered = True
 
-        # update the fields as required
-        card.title = body_data.get("title") or card.title
-        card.description = body_data.get("description") or card.description
-        card.status = body_data.get("status") or card.status
-        card.priority = body_data.get("priority") or card.priority
-        # commit to the DB
-        db.session.commit()
-        # return acknowledgement
-        return card_schema.dump(card)
-    # else
-    else:
-        # return error message
-        return {"error": f"Card with id {card_id} not found."}, 404
+card_schema = CardSchema()
+cards_schema = CardSchema(many=True)
 
